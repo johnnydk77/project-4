a27de8532f00b1156a7320bdff8b458416fae6d4
Johnny Kinnaird <johnnydk77@gmail.com>
2019-07-12 15:21:02 -0400

    landing page styled

--- wine-pair-app/client/src/Components/LandingPage.jsx
+++ wine-pair-app/client/src/Components/LandingPage.jsx
@@ -14,20 +14,32 @@ class LandingPage extends Component {
 
     }
 
-    
+
 
 
 
     render() {
         return (
             <div className="home-container">
-                <h1 className="app-title">Pair It!</h1>
-                <div className="photo-main">
-                <img src="https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="home image" height="450" width="600" />
+
+                <div className="landing-top">
+                    <h1 className="app-title">Pair It!</h1>
+                    <p className="landing-par">In these always on the go times it is difficult to know what wine pairs with that perfect meal. Pair It! Let us be your solution for making tonight's wine perfect!</p>
                 </div>
-                <p className="homepage-text">“The discovery of a wine is of greater moment than the discovery of a constellation. The universe is too full of stars.” ― Benjamin Franklin</p>
-                <button className='button-home1'><Link to="/SearchPage">Get Started!</Link></button>
-                <button className='button-wine-info'><Link to="/WineInfo">Additonal Resources</Link></button>
+
+
+                <div className="photo-main">
+                    <img src="https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="home image" className="landing-image" />
+                </div>
+
+                <div className="landing-quote">
+                    <p className="homepage-text">“The discovery of a wine is of greater moment than the discovery of a constellation. The universe is too full of stars.” ― Benjamin Franklin</p>
+                </div>
+
+                <div className="landing-buttons">
+                    <button className='button-home1'><Link to="/SearchPage" className="landing-button-push">Get Started!</Link></button>
+                    <button className='button-wine-info'><Link to="/WineInfo"  className="landing-button-push">Additonal Resources</Link></button>
+                </div>
 
             </div>
         )
--- wine-pair-app/client/src/Components/ShowWines.jsx
+++ wine-pair-app/client/src/Components/ShowWines.jsx
@@ -41,12 +41,12 @@ class ShowWines extends Component {
                         <h3 className="show-header-3">Both will only enhance your meal.</h3>
                         <h3 className="show-header-4">Your perfect Red Wine Pairing is {this.state.redWine && this.state.redWine.grape}!</h3>
                         <ul className="redwine-list">
-                            <h2>{this.state.redWine && this.state.redWine.grape}</h2>
-                            <li>{this.state.redWine && this.state.redWine.fruit}</li>
-                            <li>{this.state.redWine && this.state.redWine.flavor_profile}</li>
-                            <li>{this.state.redWine && this.state.redWine.major_regions}</li>
-                            <li>{this.state.redWine && this.state.redWine.description}</li>
-                            <img src={this.state.redWine && this.state.redWine.image_url} alt="img red" height="300" width="500" />
+                            <h2 className="show-grape">{this.state.redWine && this.state.redWine.grape}</h2>
+                            <li className="show-fruit">{this.state.redWine && this.state.redWine.fruit}</li>
+                            <li className="show-flavor">{this.state.redWine && this.state.redWine.flavor_profile}</li>
+                            <li className="show-region">{this.state.redWine && this.state.redWine.major_regions}</li>
+                            <li className="show-description">{this.state.redWine && this.state.redWine.description}</li>
+                            <img src={this.state.redWine && this.state.redWine.image_url} className="redwine-image" alt="img red" height="300" width="500" />
                         </ul>
                     </div>
                 </div>
@@ -56,12 +56,12 @@ class ShowWines extends Component {
                     <div className="column-show">
                         <h3 className="show-header-4">Your perfect White Wine Pairing is {this.state.whiteWine && this.state.whiteWine.grape}!</h3>
                         <ul className="whitewine-list">
-                            <h2>{this.state.whiteWine && this.state.whiteWine.grape}</h2>
-                            <li>{this.state.whiteWine && this.state.whiteWine.fruit}</li>
-                            <li>{this.state.whiteWine && this.state.whiteWine.flavor_profile}</li>
-                            <li>{this.state.whiteWine && this.state.whiteWine.major_regions}</li>
-                            <li>{this.state.whiteWine && this.state.whiteWine.description}</li>
-                            <img src={this.state.whiteWine && this.state.whiteWine.image_url} alt="img white" height="300" width="500" />
+                            <h2 className="show-grape">{this.state.whiteWine && this.state.whiteWine.grape}</h2>
+                            <li className="show-fruit">{this.state.whiteWine && this.state.whiteWine.fruit}</li>
+                            <li className="show-flavor">{this.state.whiteWine && this.state.whiteWine.flavor_profile}</li>
+                            <li className="show-region">{this.state.whiteWine && this.state.whiteWine.major_regions}</li>
+                            <li className="show-description">{this.state.whiteWine && this.state.whiteWine.description}</li>
+                            <img src={this.state.whiteWine && this.state.whiteWine.image_url} className="whitewine-image" alt="img white" height="300" width="500" />
 
                         </ul>
 
