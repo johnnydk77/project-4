06e07c6fab7a14d750e0ea295cef1fb01886e800
Johnny Kinnaird <johnnydk77@gmail.com>
2019-12-21 13:52:14 -0500

    now

--- client/src/Components/WineInfo.jsx
+++ client/src/Components/WineInfo.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { redWine } from './assets/redWine.png';
+import { redWine } from '../assets/redWine.png';
 
 
 
